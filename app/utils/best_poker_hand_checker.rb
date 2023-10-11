325c8f0463aef94c64e825c3e9dc6f9b6f1117de
Liu Szu Chi <sliu@netprotections.co.jp>
2023-10-11 13:56:54 +0900

    move HANDS_RANKING constant

--- app/utils/best_poker_hand_checker.rb
+++ app/utils/best_poker_hand_checker.rb
@@ -1,11 +1,21 @@
 module BestPokerHandChecker
+  HANDS_RANKING = { 'ストレートフラッシュ' => 9,
+                    'フォー・オブ・ア・カインド' => 8,
+                    'フルハウス' => 7,
+                    'フラッシュ' => 6,
+                    'ストレート' => 5,
+                    'スリー・オブ・ア・カインド' => 4,
+                    'ツーペア' => 3,
+                    'ペア' => 2,
+                    'ハイカード' => 1 }
+
   def self.check_best_poker_hand(card_sets)
     card_set_score = card_sets.map do |card_set|
       card_set_array = card_set.split
       hand_type = PokerHand.new(card_set_array).evaluate_hand_type
       {
         card_set: card_set,
-        score: PokerHand::HANDS_RANKING[hand_type]
+        score: HANDS_RANKING[hand_type]
       }
     end
 
