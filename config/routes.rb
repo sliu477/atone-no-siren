42096eb37075001943f6deebcd856cd1cb73860a
Liu Szu Chi <sliu@netprotections.co.jp>
2023-10-17 11:01:31 +0900

    mod route name

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get "/poker_checker" => "poker_hand_checker#index"
-  post "poker_checker/check" => "poker_hand_checker#check"
+  post "poker_checker" => "poker_hand_checker#check"
   # grape api
   mount API::Root => "/"
 end
