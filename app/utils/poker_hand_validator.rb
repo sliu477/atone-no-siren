1c1a259be200006756e039d10b63b035ba3be056
Liu Szu Chi <sliu@netprotections.co.jp>
2023-10-17 14:50:56 +0900

    mod card validation

--- app/utils/poker_hand_validator.rb
+++ app/utils/poker_hand_validator.rb
@@ -1,34 +1,45 @@
 module PokerHandValidator
-
   def self.validate_single_card_set(card_set)
     cards_arr = card_set.split
-    if invalid_num_of_cards?(cards_arr)
-      ["5つのカード指定文字を半角スペース区切りで入力してください。"]
-    elsif duplicate_cards?(cards_arr)
-      ["カードが重複しています。"]
-    else
-      check_typo_card_index(cards_arr)
-    end
+    error_messages = []
+
+    error_messages << invalid_num_of_cards_error(cards_arr)
+    error_messages << duplicate_cards_error(cards_arr)
+    error_messages << typo_card_error(cards_arr)
+
+    error_messages.compact
   end
 
-  private
-
-  def self.invalid_num_of_cards?(cards_arr)
-    cards_arr.length != 5
+  def self.invalid_num_of_cards_error(cards_arr)
+    '5つのカード指定文字を半角スペース区切りで入力してください。' if cards_arr.length != 5
   end
 
-  def self.duplicate_cards?(cards_arr)
-    cards_arr != cards_arr.uniq
+  def self.duplicate_cards_error(cards_arr)
+    duplicate_cards = cards_arr.find_all { |e| cards_arr.count(e) > 1 }.uniq
+    "カードが重複しています。(#{duplicate_cards.join(' ')})" if duplicate_cards.any?
   end
 
-  def self.check_typo_card_index(card_arr)
+  def self.typo_card_error(cards_arr)
     poker_card_regex = /^([SHDC])([1-9]|1[0-3])$/
-    error_msg_arr = []
-
-    card_arr.each.with_index(1) do |card, i|
-      error_msg_arr.push("#{i}番目のカード指定文字が不正です。(#{card})") if card !~ poker_card_regex
+    error_indices = []
+    error_cards = []
+    cards_arr.each.with_index(1) do |card, i|
+      if card !~ poker_card_regex
+        error_indices << i
+        error_cards << card
+      end
     end
-    error_msg_arr.blank? ? nil : error_msg_arr
-  end
 
+    return if error_indices.blank?
+
+    error_message = case error_indices.size
+                    when 1
+                      "#{error_indices[0]}番目のカード指定文字が不正です。"
+                    when 2
+                      "#{error_indices[0]}番目と#{error_indices[1]}番目のカード指定文字が不正です。"
+                    else
+                      "#{error_indices[0..-2].join('番目、')}#{error_indices[-1]}番目のカード指定文字が不正です。)"
+                    end
+    error_message = "#{error_message}(#{error_cards.join(' ')})"
+  end
 end
